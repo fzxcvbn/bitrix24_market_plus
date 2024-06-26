71b23fc0bebf6593f40378be622235fc36c5e650
Diana <160869579+fzxcvbn@users.noreply.github.com>
2024-06-26 23:04:42 +0300

    почти доделала

---
+++ index.js
@@ -0,0 +1,117 @@
+let users_content_bitrix24_market_plus = document.querySelector(".users-content-bitrix24-market-plus"),
+    users_enterprice_bitrix24_market_plus = document.querySelector(".users-enterprice-bitrix24-market-plus")
+
+
+function change_user_bitrix24() {
+    users_content_bitrix24_market_plus.style.display = 'block'
+    users_enterprice_bitrix24_market_plus.style.backgroundColor = 'white'
+    users_enterprice_bitrix24_market_plus.style.border = 'none'
+}
+
+function users250_bitrix24() {
+    price_enterprice_bitrix24.textContent = '33 990 ₽/мес'
+    users_content_bitrix24.style.display = 'none'
+    user_enterprice_bitrix24.textContent = '250'
+    users_enterprice_bitrix24.style.backgroundColor = 'rgb(242, 242, 242)'
+    users_enterprice_bitrix24.style.border = '2px solid grey'
+    gb_enterprice_bitrix24.textContent = '3 ТБ'
+}
+
+function users500_bitrix24() {
+    price_enterprice_bitrix24.textContent = '59 990 ₽/мес'
+    users_content_bitrix24.style.display = 'none'
+    user_enterprice_bitrix24.textContent = '500'
+    users_enterprice_bitrix24.style.backgroundColor = 'rgb(242, 242, 242)'
+    users_enterprice_bitrix24.style.border = '2px solid grey'
+    gb_enterprice_bitrix24.textContent = '5 ТБ'
+}
+
+function users1000_bitrix24() {
+    price_enterprice_bitrix24.textContent = '99 990 ₽/мес'
+    users_content_bitrix24.style.display = 'none'
+    user_enterprice_bitrix24.textContent = '1000'
+    users_enterprice_bitrix24.style.backgroundColor = 'rgb(242, 242, 242)'
+    users_enterprice_bitrix24.style.border = '2px solid grey'
+    gb_enterprice_bitrix24.textContent = '10 ТБ'
+}
+
+function users2000_bitrix24() {
+    price_enterprice_bitrix24.textContent = '199 990 ₽/мес'
+    users_content_bitrix24.style.display = 'none'
+    user_enterprice_bitrix24.textContent = '2000'
+    users_enterprice_bitrix24.style.backgroundColor = 'rgb(242, 242, 242)'
+    users_enterprice_bitrix24.style.border = '2px solid grey'
+    gb_enterprice_bitrix24.textContent = '20 ТБ'
+}
+
+function users3000_bitrix24() {
+    price_enterprice_bitrix24.textContent = '299 990 ₽/мес'
+    users_content_bitrix24.style.display = 'none'
+    user_enterprice_bitrix24.textContent = '3000'
+    users_enterprice_bitrix24.style.backgroundColor = 'rgb(242, 242, 242)'
+    users_enterprice_bitrix24.style.border = '2px solid grey'
+    gb_enterprice_bitrix24.textContent = '30 ТБ'
+}
+
+function users4000_bitrix24() {
+    price_enterprice_bitrix24.textContent = '399 990 ₽/мес'
+    users_content_bitrix24.style.display = 'none'
+    user_enterprice_bitrix24.textContent = '4000'
+    users_enterprice_bitrix24.style.backgroundColor = 'rgb(242, 242, 242)'
+    users_enterprice_bitrix24.style.border = '2px solid grey'
+    gb_enterprice_bitrix24.textContent = '40 ТБ'
+}
+
+function users5000_bitrix24() {
+    price_enterprice_bitrix24.textContent = '499 990 ₽/мес'
+    users_content_bitrix24.style.display = 'none'
+    user_enterprice_bitrix24.textContent = '5000'
+    users_enterprice_bitrix24.style.backgroundColor = 'rgb(242, 242, 242)'
+    users_enterprice_bitrix24.style.border = '2px solid grey'
+    gb_enterprice_bitrix24.textContent = '50 ТБ'
+}
+
+function users6000_bitrix24() {
+    price_enterprice_bitrix24.textContent = '599 990 ₽/мес'
+    users_content_bitrix24.style.display = 'none'
+    user_enterprice_bitrix24.textContent = '6000'
+    users_enterprice_bitrix24.style.backgroundColor = 'rgb(242, 242, 242)'
+    users_enterprice_bitrix24.style.border = '2px solid grey'
+    gb_enterprice_bitrix24.textContent = '60 ТБ'
+}
+
+function users7000_bitrix24() {
+    price_enterprice_bitrix24.textContent = '699 990 ₽/мес'
+    users_content_bitrix24.style.display = 'none'
+    user_enterprice_bitrix24.textContent = '7000'
+    users_enterprice_bitrix24.style.backgroundColor = 'rgb(242, 242, 242)'
+    users_enterprice_bitrix24.style.border = '2px solid grey'
+    gb_enterprice_bitrix24.textContent = '70 ТБ'
+}
+
+function users8000_bitrix24() {
+    price_enterprice_bitrix24.textContent = '799 990 ₽/мес'
+    users_content_bitrix24.style.display = 'none'
+    user_enterprice_bitrix24.textContent = '8000'
+    users_enterprice_bitrix24.style.backgroundColor = 'rgb(242, 242, 242)'
+    users_enterprice_bitrix24.style.border = '2px solid grey'
+    gb_enterprice_bitrix24.textContent = '80 ТБ'
+}
+
+function users9000_bitrix24() {
+    price_enterprice_bitrix24.textContent = '899 990 ₽/мес'
+    users_content_bitrix24.style.display = 'none'
+    user_enterprice_bitrix24.textContent = '9000'
+    users_enterprice_bitrix24.style.backgroundColor = 'rgb(242, 242, 242)'
+    users_enterprice_bitrix24.style.border = '2px solid grey'
+    gb_enterprice_bitrix24.textContent = '90 ТБ'
+}
+
+function users10000_bitrix24() {
+    price_enterprice_bitrix24.textContent = '999 990 ₽/мес'
+    users_content_bitrix24.style.display = 'none'
+    user_enterprice_bitrix24.textContent = '10000'
+    users_enterprice_bitrix24.style.backgroundColor = 'rgb(242, 242, 242)'
+    users_enterprice_bitrix24.style.border = '2px solid grey'
+    gb_enterprice_bitrix24.textContent = '100 ТБ'
+}
