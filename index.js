59b5cc3b1542d010dad24b8eb01c154d7cd7c10d
Diana <160869579+fzxcvbn@users.noreply.github.com>
2024-06-27 18:40:58 +0300

    Изменеения

--- index.js
+++ index.js
@@ -1,117 +1,120 @@
 let users_content_bitrix24_market_plus = document.querySelector(".users-content-bitrix24-market-plus"),
-    users_enterprice_bitrix24_market_plus = document.querySelector(".users-enterprice-bitrix24-market-plus")
+    change_users_bitrix24_market_plus = document.querySelector(".change-users-bitrix24-market-plus"),
+    count_users_enterprice_bitrix24_market_plus = document.querySelector(".count-users-enterprice-bitrix24-market-plus")
+    nav_month_bitrix24_market_plus = document.querySelector(".nav-month-bitrix24-market-plus"),
+    nav_year_bitrix24_market_plus = document.querySelector(".nav-year-bitrix24-market-plus"),
+    s_text_bitrix24_market_plus = document.querySelectorAll(".s-text-bitrix24-market-plus"),
+    discount_bitrix24_market_plus = document.querySelectorAll(".discount-bitrix24-market-plus")
+    i = 0
 
+nav_month_bitrix24_market_plus.addEventListener("click", function() {
+    nav_month_bitrix24_market_plus.style.backgroundColor = '#30d4c7'
+    nav_month_bitrix24_market_plus.style.color = 'white'
+    nav_year_bitrix24_market_plus.style.backgroundColor = 'white'
+    nav_year_bitrix24_market_plus.style.color = '#30d4c7'
+    for (i = 0; i < 4; i++) {
+        s_text_bitrix24_market_plus[i].style.display = "none";
+        discount_bitrix24_market_plus[i].style.display = "none";
+    }
+})
 
-function change_user_bitrix24() {
+nav_year_bitrix24_market_plus.addEventListener("click", function() {
+    nav_year_bitrix24_market_plus.style.backgroundColor = '#30d4c7'
+    nav_year_bitrix24_market_plus.style.color = 'white'
+    nav_month_bitrix24_market_plus.style.backgroundColor = 'white'
+    nav_month_bitrix24_market_plus.style.color = '#30d4c7'
+    for (i = 0; i < s_text_bitrix24_market_plus.length; i++) {
+        s_text_bitrix24_market_plus[i].style.display = "block";
+        discount_bitrix24_market_plus[i].style.display = "block";
+    }
+})
+
+function change_user_bitrix24_market_plus() {
     users_content_bitrix24_market_plus.style.display = 'block'
-    users_enterprice_bitrix24_market_plus.style.backgroundColor = 'white'
-    users_enterprice_bitrix24_market_plus.style.border = 'none'
+    change_users_bitrix24_market_plus.style.backgroundColor = 'white'
+    change_users_bitrix24_market_plus.style.border = 'none'
 }
 
 function users250_bitrix24() {
-    price_enterprice_bitrix24.textContent = '33 990 ₽/мес'
-    users_content_bitrix24.style.display = 'none'
-    user_enterprice_bitrix24.textContent = '250'
-    users_enterprice_bitrix24.style.backgroundColor = 'rgb(242, 242, 242)'
-    users_enterprice_bitrix24.style.border = '2px solid grey'
-    gb_enterprice_bitrix24.textContent = '3 ТБ'
+    users_content_bitrix24_market_plus.style.display = 'none'
+    count_users_enterprice_bitrix24_market_plus.textContent = '250'
+    change_users_bitrix24_market_plus.style.backgroundColor = 'rgb(242, 242, 242)'
+    change_users_bitrix24_market_plus.style.border = '2px solid grey'
 }
 
 function users500_bitrix24() {
-    price_enterprice_bitrix24.textContent = '59 990 ₽/мес'
-    users_content_bitrix24.style.display = 'none'
-    user_enterprice_bitrix24.textContent = '500'
-    users_enterprice_bitrix24.style.backgroundColor = 'rgb(242, 242, 242)'
-    users_enterprice_bitrix24.style.border = '2px solid grey'
-    gb_enterprice_bitrix24.textContent = '5 ТБ'
+    users_content_bitrix24_market_plus.style.display = 'none'
+    count_users_enterprice_bitrix24_market_plus.textContent = '500'
+    change_users_bitrix24_market_plus.style.backgroundColor = 'rgb(242, 242, 242)'
+    change_users_bitrix24_market_plus.style.border = '2px solid grey'
 }
 
 function users1000_bitrix24() {
-    price_enterprice_bitrix24.textContent = '99 990 ₽/мес'
-    users_content_bitrix24.style.display = 'none'
-    user_enterprice_bitrix24.textContent = '1000'
-    users_enterprice_bitrix24.style.backgroundColor = 'rgb(242, 242, 242)'
-    users_enterprice_bitrix24.style.border = '2px solid grey'
-    gb_enterprice_bitrix24.textContent = '10 ТБ'
+    users_content_bitrix24_market_plus.style.display = 'none'
+    count_users_enterprice_bitrix24_market_plus.textContent = '1000'
+    change_users_bitrix24_market_plus.style.backgroundColor = 'rgb(242, 242, 242)'
+    change_users_bitrix24_market_plus.style.border = '2px solid grey'
 }
 
 function users2000_bitrix24() {
-    price_enterprice_bitrix24.textContent = '199 990 ₽/мес'
-    users_content_bitrix24.style.display = 'none'
-    user_enterprice_bitrix24.textContent = '2000'
-    users_enterprice_bitrix24.style.backgroundColor = 'rgb(242, 242, 242)'
-    users_enterprice_bitrix24.style.border = '2px solid grey'
-    gb_enterprice_bitrix24.textContent = '20 ТБ'
+    users_content_bitrix24_market_plus.style.display = 'none'
+    count_users_enterprice_bitrix24_market_plus.textContent = '2000'
+    change_users_bitrix24_market_plus.style.backgroundColor = 'rgb(242, 242, 242)'
+    change_users_bitrix24_market_plus.style.border = '2px solid grey'
 }
 
 function users3000_bitrix24() {
-    price_enterprice_bitrix24.textContent = '299 990 ₽/мес'
-    users_content_bitrix24.style.display = 'none'
-    user_enterprice_bitrix24.textContent = '3000'
-    users_enterprice_bitrix24.style.backgroundColor = 'rgb(242, 242, 242)'
-    users_enterprice_bitrix24.style.border = '2px solid grey'
-    gb_enterprice_bitrix24.textContent = '30 ТБ'
+    users_content_bitrix24_market_plus.style.display = 'none'
+    count_users_enterprice_bitrix24_market_plus.textContent = '3000'
+    change_users_bitrix24_market_plus.style.backgroundColor = 'rgb(242, 242, 242)'
+    change_users_bitrix24_market_plus.style.border = '2px solid grey'
 }
 
 function users4000_bitrix24() {
-    price_enterprice_bitrix24.textContent = '399 990 ₽/мес'
-    users_content_bitrix24.style.display = 'none'
-    user_enterprice_bitrix24.textContent = '4000'
-    users_enterprice_bitrix24.style.backgroundColor = 'rgb(242, 242, 242)'
-    users_enterprice_bitrix24.style.border = '2px solid grey'
-    gb_enterprice_bitrix24.textContent = '40 ТБ'
+    users_content_bitrix24_market_plus.style.display = 'none'
+    count_users_enterprice_bitrix24_market_plus.textContent = '4000'
+    change_users_bitrix24_market_plus.style.backgroundColor = 'rgb(242, 242, 242)'
+    change_users_bitrix24_market_plus.style.border = '2px solid grey'
 }
 
 function users5000_bitrix24() {
-    price_enterprice_bitrix24.textContent = '499 990 ₽/мес'
-    users_content_bitrix24.style.display = 'none'
-    user_enterprice_bitrix24.textContent = '5000'
-    users_enterprice_bitrix24.style.backgroundColor = 'rgb(242, 242, 242)'
-    users_enterprice_bitrix24.style.border = '2px solid grey'
-    gb_enterprice_bitrix24.textContent = '50 ТБ'
+    users_content_bitrix24_market_plus.style.display = 'none'
+    count_users_enterprice_bitrix24_market_plus.textContent = '5000'
+    change_users_bitrix24_market_plus.style.backgroundColor = 'rgb(242, 242, 242)'
+    change_users_bitrix24_market_plus.style.border = '2px solid grey'
 }
 
 function users6000_bitrix24() {
-    price_enterprice_bitrix24.textContent = '599 990 ₽/мес'
-    users_content_bitrix24.style.display = 'none'
-    user_enterprice_bitrix24.textContent = '6000'
-    users_enterprice_bitrix24.style.backgroundColor = 'rgb(242, 242, 242)'
-    users_enterprice_bitrix24.style.border = '2px solid grey'
-    gb_enterprice_bitrix24.textContent = '60 ТБ'
+    users_content_bitrix24_market_plus.style.display = 'none'
+    count_users_enterprice_bitrix24_market_plus.textContent = '6000'
+    change_users_bitrix24_market_plus.style.backgroundColor = 'rgb(242, 242, 242)'
+    change_users_bitrix24_market_plus.style.border = '2px solid grey'
 }
 
 function users7000_bitrix24() {
-    price_enterprice_bitrix24.textContent = '699 990 ₽/мес'
-    users_content_bitrix24.style.display = 'none'
-    user_enterprice_bitrix24.textContent = '7000'
-    users_enterprice_bitrix24.style.backgroundColor = 'rgb(242, 242, 242)'
-    users_enterprice_bitrix24.style.border = '2px solid grey'
-    gb_enterprice_bitrix24.textContent = '70 ТБ'
+    users_content_bitrix24_market_plus.style.display = 'none'
+    count_users_enterprice_bitrix24_market_plus.textContent = '7000'
+    change_users_bitrix24_market_plus.style.backgroundColor = 'rgb(242, 242, 242)'
+    change_users_bitrix24_market_plus.style.border = '2px solid grey'
 }
 
 function users8000_bitrix24() {
-    price_enterprice_bitrix24.textContent = '799 990 ₽/мес'
-    users_content_bitrix24.style.display = 'none'
-    user_enterprice_bitrix24.textContent = '8000'
-    users_enterprice_bitrix24.style.backgroundColor = 'rgb(242, 242, 242)'
-    users_enterprice_bitrix24.style.border = '2px solid grey'
-    gb_enterprice_bitrix24.textContent = '80 ТБ'
+    users_content_bitrix24_market_plus.style.display = 'none'
+    count_users_enterprice_bitrix24_market_plus.textContent = '8000'
+    change_users_bitrix24_market_plus.style.backgroundColor = 'rgb(242, 242, 242)'
+    change_users_bitrix24_market_plus.style.border = '2px solid grey'
 }
 
 function users9000_bitrix24() {
-    price_enterprice_bitrix24.textContent = '899 990 ₽/мес'
-    users_content_bitrix24.style.display = 'none'
-    user_enterprice_bitrix24.textContent = '9000'
-    users_enterprice_bitrix24.style.backgroundColor = 'rgb(242, 242, 242)'
-    users_enterprice_bitrix24.style.border = '2px solid grey'
-    gb_enterprice_bitrix24.textContent = '90 ТБ'
+    users_content_bitrix24_market_plus.style.display = 'none'
+    count_users_enterprice_bitrix24_market_plus.textContent = '9000'
+    change_users_bitrix24_market_plus.style.backgroundColor = 'rgb(242, 242, 242)'
+    change_users_bitrix24_market_plus.style.border = '2px solid grey'
 }
 
 function users10000_bitrix24() {
-    price_enterprice_bitrix24.textContent = '999 990 ₽/мес'
-    users_content_bitrix24.style.display = 'none'
-    user_enterprice_bitrix24.textContent = '10000'
-    users_enterprice_bitrix24.style.backgroundColor = 'rgb(242, 242, 242)'
-    users_enterprice_bitrix24.style.border = '2px solid grey'
-    gb_enterprice_bitrix24.textContent = '100 ТБ'
+    users_content_bitrix24_market_plus.style.display = 'none'
+    count_users_enterprice_bitrix24_market_plus.textContent = '10000'
+    change_users_bitrix24_market_plus.style.backgroundColor = 'rgb(242, 242, 242)'
+    change_users_bitrix24_market_plus.style.border = '2px solid grey'
 }
